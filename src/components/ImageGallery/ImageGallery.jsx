0a858790f5908daca1383264ea44b36bc4e8e831
Andrii Ivaniuk <andrey.ivanjuk@gmail.com>
2023-08-02 17:18:40 +0300

    props types added

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,28 +1,40 @@
-import {ImageGalleryItem} from '../ImageGalleryItem/ImageGalleryItem'
-import {Button} from '../Button/Button'
-import {Loader} from '../Loader/Loader'
+import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
+import { Button } from '../Button/Button';
+import { Loader } from '../Loader/Loader';
 import PropTypes from 'prop-types';
 
-import IGCss from './ImageGallery.module.css'
+import IGCss from './ImageGallery.module.css';
 
-export function ImageGallery ({addMore, openModal, imageBase, total, load}) {
+export function ImageGallery({ addMore, openModal, imageBase, total, load }) {
+  return (
+    <>
+      <ul className={IGCss.ImageGallery}>
+        {imageBase.map(({ id, webformatURL, tags, largeImageURL }) => {
+          return (
+            <ImageGalleryItem
+              key={id}
+              webformatURL={webformatURL}
+              tags={tags}
+              imageForModal={largeImageURL}
+              openModal={openModal}
+            />
+          );
+        })}
+      </ul>
 
-    return <>
-                
-                <ul className={IGCss.ImageGallery}>
-                    {imageBase.map(({id, webformatURL, tags, largeImageURL}) => {
-                        return <ImageGalleryItem key={id} webformatURL={webformatURL} tags={tags} imageForModal={largeImageURL} openModal={openModal}/>
-                    })}
-                </ul>
+      {load && <Loader />}
 
-                {load && <Loader />}
-
-                { imageBase.length > 0 && imageBase.length < total && <Button addMore={addMore}/>} 
-                
-             
+      {imageBase.length > 0 && imageBase.length < total && (
+        <Button addMore={addMore} />
+      )}
     </>
+  );
 }
 
 ImageGallery.propTypes = {
-    openModal:PropTypes.func.isRequired,
-}
+  openModal: PropTypes.func.isRequired,
+  addMore: PropTypes.func.isRequired,
+  imageBase: PropTypes.array.isRequired,
+  load: PropTypes.bool.isRequired,
+  total: PropTypes.number.isRequired,
+};
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,35 +1,34 @@
-import { useEffect } from 'react'
-import modalCss from './Modal.module.css'
+import { useEffect } from 'react';
+import modalCss from './Modal.module.css';
 import PropTypes from 'prop-types';
 
-export function Modal ({imageModal, tagModal, closeModal}) {
+export function Modal({ imageModal, tagModal, closeModal }) {
+  useEffect(() => {
+    const handleESC = e => {
+      if (e.code === 'Escape') {
+        closeModal();
+      }
+    };
 
-    useEffect(() => {
-        const handleESC = (e) => {
-            if (e.code === 'Escape') {
-                closeModal()
-            }
-        } 
+    document.addEventListener('keydown', handleESC);
+    return () => {
+      document.removeEventListener('keydown', handleESC);
+    };
+  }, [closeModal]);
 
-        document.addEventListener('keydown', handleESC)
-        return () => {
-            document.removeEventListener('keydown', handleESC)
-        }
-    },[closeModal])
-    
-    
-
-
-        return <>
-            <div className={modalCss.Overlay} onClick={closeModal}>
-                <div className={modalCss.Modal}>
-                    <img src={imageModal} alt={tagModal} />
-                </div>
-            </div>
-        </>
-    
+  return (
+    <>
+      <div className={modalCss.Overlay} onClick={closeModal}>
+        <div className={modalCss.Modal}>
+          <img src={imageModal} alt={tagModal} />
+        </div>
+      </div>
+    </>
+  );
 }
 
 Modal.propTypes = {
-    closeModal: PropTypes.func.isRequired,
-}
+  closeModal: PropTypes.func.isRequired,
+  imageModal: PropTypes.string.isRequired,
+  tagModal: PropTypes.string,
+};
